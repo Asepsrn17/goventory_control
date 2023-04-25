beeed5cc891ca3f96a1830b2468c289a9fabf014
uchel09 <ucelrussel77@gmail.com>
2023-04-25 16:11:24 +0900

    membersihkan comment di config

--- config/config.go
+++ config/config.go
@@ -6,15 +6,6 @@ import (
 	"go_inven_ctrl/utils"
 )
 
-// const (
-// 	host     = "localhost"
-// 	port     = 5432
-// 	user     = "postgres"
-// 	password = "1w551474"
-// 	dbname   = "testing"
-// )
-
-// config db
 var (
 	dbHost     = utils.DotEnv("DB_HOST")
 	dbPort     = utils.DotEnv("DB_PORT")
@@ -27,15 +18,7 @@ var (
 // db connection
 var dataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", dbHost, dbPort, dbUser, dbPassword, dbName, sslMode)
 
-// db, err := sql.Open("postgres", dataSourceName)
-
-// koneksi database ================================================================================
-
-// var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-// 	host, port, user, password, dbname)
-
 func ConnectDB() *sql.DB {
-	// db, err := sql.Open("postgres", psqlInfo)
 	db, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
 		panic(err)
